6fb4039f185afeae9a2af03f74b8f7c201225020
pavan7710 <bollinenipavan24111997@gmail.com>
2021-05-22 18:48:21 +0530

    dummy data was added in master branch

--- NEMESIS/server.js
+++ NEMESIS/server.js
@@ -84,6 +84,8 @@ app.post('/update/:id' , (req , res) => {
     .catch(err => res.status(400).json(err))
 })
 
+// Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+
 app.listen(port , err => {
     if (err) throw err 
     console.log(`server is running at ${port}`)
